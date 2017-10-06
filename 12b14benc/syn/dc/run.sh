29a26dcc35e3d38ed6ef0545b775f238283e86e2
Faisal Abu-Nimeh <abunimeh@slac.stanford.edu>
2017-10-06 16:47:41 -0700

    add shellscript to execute dc flow

---
+++ 12b14benc/syn/dc/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env tcsh
+#
+# Script to synthesize ssp 12b14b encoder using Synopsys DC
+# Faisal T. Abu-Nimeh 20171005
+
+# load vcs env variables
+source /afs/slac/g/reseng/synopsys/syn/M-2016.12-SP5-1/settings.csh
+
+# dc flow is in rtl.tcl
+dc_shell-xg-t -f rtl.tcl
