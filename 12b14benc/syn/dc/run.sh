05e7223c8d6357ee0d57e3870e55a6c14687f3d4
Faisal Abu-Nimeh <abunimeh@slac.stanford.edu>
2017-10-09 09:12:30 -0700

    add PDK path env var

--- 12b14benc/syn/dc/run.sh
+++ 12b14benc/syn/dc/run.sh
@@ -5,6 +5,7 @@
 
 # load vcs env variables
 source /afs/slac/g/reseng/synopsys/syn/M-2016.12-SP5-1/settings.csh
+setenv TSMC130_DIR /u1/abunimeh/pdk/TSMCHOME/digital
 
 # dc flow is in rtl.tcl
 dc_shell-xg-t -f rtl.tcl
