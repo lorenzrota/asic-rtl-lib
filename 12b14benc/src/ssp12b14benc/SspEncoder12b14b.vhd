afedb4ad2e5031308030dab8cd65cfbedd9e8ca4
Faisal Abu-Nimeh <abunimeh@slac.stanford.edu>
2017-10-11 12:58:39 -0700

    fix naming convention, remove useless delay and ports, and reformat

--- 12b14benc/src/ssp12b14benc/SspEncoder12b14b.vhd
+++ 12b14benc/src/ssp12b14benc/SspEncoder12b14b.vhd
@@ -9,11 +9,11 @@
 -- ties the framing core to an RTL 12b14b encoder.
 -------------------------------------------------------------------------------
 -- This file is part of 'SLAC Firmware Standard Library'.
--- It is subject to the license terms in the LICENSE.txt file found in the 
--- top-level directory of this distribution and at: 
---    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
--- No part of 'SLAC Firmware Standard Library', including this file, 
--- may be copied, modified, propagated, or distributed except according to 
+-- It is subject to the license terms in the LICENSE.txt file found in the
+-- top-level directory of this distribution and at:
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+-- No part of 'SLAC Firmware Standard Library', including this file,
+-- may be copied, modified, propagated, or distributed except according to
 -- the terms contained in the LICENSE.txt file.
 -------------------------------------------------------------------------------
 
@@ -26,80 +26,72 @@ use work.StdRtlPkg.all;
 use work.Code12b14bPkg.all;
 
 entity SspEncoder12b14b is
+    generic(
+        RST_POLARITY_G : sl      := '1';
+        RST_ASYNC_G    : boolean := true;
+        AUTO_FRAME_G   : boolean := true;
+        FLOW_CTRL_EN_G : boolean := false);
+    port(
+        clk_i   : in  std_logic;
+        rst_i   : in  std_logic := RST_POLARITY_G;
+        valid_i : in  std_logic;
+        data_i  : in  std_logic_vector(11 downto 0);
 
-   generic (
-      TPD_G          : time    := 1 ns;
-      RST_POLARITY_G : sl      := '0';
-      RST_ASYNC_G    : boolean := true;
-      AUTO_FRAME_G   : boolean := true;
-      FLOW_CTRL_EN_G : boolean := false);
-   port (
-      clk      : in  sl;
-      rst      : in  sl := RST_POLARITY_G;
-      validIn  : in  sl;
-      readyIn  : out sl;
-      sof      : in  sl := '0';
-      eof      : in  sl := '0';
-      dataIn   : in  slv(11 downto 0);
-      validOut : out sl;
-      readyOut : in  sl := '1';
-      dataOut  : out slv(13 downto 0));
-
+        valid_o : out std_logic;
+        ready_o : out std_logic;
+        data_o  : out std_logic_vector(13 downto 0)
+    );
 end entity SspEncoder12b14b;
 
 architecture rtl of SspEncoder12b14b is
 
-   signal readyOutInt : sl;
-   signal framedData  : slv(11 downto 0) := (others => '0');
-   signal framedDataK : slv(0 downto 0)  := (others => '0');
-   signal validInt    : sl;
-   signal readyInt    : sl;
+    signal s_frame  : std_logic_vector(11 downto 0) := (others => '0');
+    signal s_framek : std_logic_vector(0 downto 0)  := (others => '0');
+    signal s_valid  : std_logic;
+    signal s_ready  : std_logic;
 
 begin
 
-   SspFramer_1 : entity work.SspFramer
-      generic map (
-         TPD_G           => TPD_G,
-         RST_POLARITY_G  => RST_POLARITY_G,
-         RST_ASYNC_G     => RST_ASYNC_G,
-         AUTO_FRAME_G    => AUTO_FRAME_G,
-         FLOW_CTRL_EN_G  => FLOW_CTRL_EN_G,
-         WORD_SIZE_G     => 12,
-         K_SIZE_G        => 1,
-         SSP_IDLE_CODE_G => K_120_11_C,
-         SSP_IDLE_K_G    => "1",
-         SSP_SOF_CODE_G  => K_120_0_C,
-         SSP_SOF_K_G     => "1",
-         SSP_EOF_CODE_G  => K_120_1_C,
-         SSP_EOF_K_G     => "1")
-      port map (
-         clk      => clk,
-         rst      => rst,
-         validIn  => validIn,
-         readyIn  => readyIn,
-         sof      => sof,
-         eof      => eof,
-         dataIn   => dataIn,
-         validOut => validInt,
-         readyOut => readyInt,
-         dataOut  => framedData,
-         dataKOut => framedDataK);
+    SspFramer_1 : entity work.SspFramer
+        generic map(
+            RST_POLARITY_G  => RST_POLARITY_G,
+            RST_ASYNC_G     => RST_ASYNC_G,
+            AUTO_FRAME_G    => AUTO_FRAME_G,
+            FLOW_CTRL_EN_G  => FLOW_CTRL_EN_G,
+            WORD_SIZE_G     => 12,
+            K_SIZE_G        => 1,
+            SSP_IDLE_CODE_G => K_120_11_C,
+            SSP_IDLE_K_G    => "1",
+            SSP_SOF_CODE_G  => K_120_0_C,
+            SSP_SOF_K_G     => "1",
+            SSP_EOF_CODE_G  => K_120_1_C,
+            SSP_EOF_K_G     => "1")
+        port map(
+            clk      => clk_i,
+            rst      => rst_i,
+            validIn  => valid_i,
+            readyIn  => ready_o,
+            dataIn   => data_i,
+            validOut => s_valid,
+            readyOut => s_ready,
+            dataOut  => s_frame,
+            dataKOut => s_framek
+        );
 
-   Encoder12b14b_1 : entity work.Encoder12b14b
-      generic map (
-         TPD_G          => TPD_G,
-         RST_POLARITY_G => RST_POLARITY_G,
-         RST_ASYNC_G    => RST_ASYNC_G,
-         FLOW_CTRL_EN_G => FLOW_CTRL_EN_G)
-      port map (
-         clk      => clk,
-         rst      => rst,
-         validIn  => validInt,
-         readyIn  => readyInt,
-         dataIn   => framedData,
-         dataKIn  => framedDataK(0),
-         validOut => validOut,
-         readyOut => readyOutInt,
-         dataOut  => dataOut);
+    Encoder12b14b_1 : entity work.Encoder12b14b
+        generic map(
+            RST_POLARITY_G => RST_POLARITY_G,
+            RST_ASYNC_G    => RST_ASYNC_G,
+            FLOW_CTRL_EN_G => FLOW_CTRL_EN_G)
+        port map(
+            clk      => clk_i,
+            rst      => rst_i,
+            validIn  => s_valid,
+            readyIn  => s_ready,
+            dataIn   => s_frame,
+            dataKIn  => s_framek(0),
+            validOut => valid_o,
+            dataOut  => data_o
+        );
 
 end architecture rtl;
