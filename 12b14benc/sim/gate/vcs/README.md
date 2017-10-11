be7ea18f96b4b035bae01575c35a12e327fdcfda
Faisal Abu-Nimeh <abunimeh@slac.stanford.edu>
2017-10-11 12:54:22 -0700

    add gate-level testbench files

---
+++ 12b14benc/sim/gate/vcs/README.md
@@ -0,0 +1,9 @@
+This is a script to simulate and test 12b14b encoder rtl using Synopsys VCS.
+
+Simply execute ```./run.sh``` followed by either:
+
+```vcs ssp12b14b_g_tb -debug_all -R -gui ```
+
+or
+
+```./simv```
---
+++ 12b14benc/sim/gate/vcs/run.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env tcsh
+#
+# Script to simulate and test 12b14b encoder using Synopsys VCS
+# Faisal T. Abu-Nimeh 20171005
+
+# load vcs env variables
+source /afs/slac/g/reseng/synopsys/vcs-mx/M-2017.03-SP2/settings.csh
+setenv TSMC130_DIR /u1/abunimeh/pdk/TSMCHOME/digital
+
+# clean up some files
+rm -rf wlib simv* csrc DVEfiles inter.vpd ucli.key .vlogansetup.args
+rm -rf .vlogansetup.env
+# create work dir
+mkdir -p wlib
+
+# compiler arguments
+set cargs='-nc' # common arguments
+set src=../../../src/ssp12b14benc
+set vsrc=../../../src/verilog-lfsr/rtl
+set gsrc=../../../syn/dc
+# compile vhd files
+vhdlan $cargs $src/StdRtlPkg.vhd
+vhdlan $cargs $src/Code12b14bPkg.vhd
+vhdlan $cargs $src/Encoder12b14b.vhd
+vhdlan $cargs $src/SspFramer.vhd
+vhdlan $cargs $src/SspDeframer.vhd
+vhdlan $cargs $src/Decoder12b14b.vhd
+vhdlan $cargs $src/SspDecoder12b14b.vhd
+# vhdlan $cargs $src/SspEncoder12b14b.vhd
+vhdlan $cargs $src/syncbus.vhd
+
+# compile verilog files
+vlogan $cargs $vsrc/lfsr.v
+vlogan $cargs $vsrc/lfsr_prbs_gen.v
+# std verilog
+vlogan $cargs $TSMC130_DIR/Front_End/verilog/tcb013ghp_220a/tcb013ghp.v
+# gate-level file
+vlogan $cargs $gsrc/enc.v
+
+# This is the top
+vhdlan $cargs $src/ssp12b14b_g_tb.vhd
+
+# run the testbench
+vcs $cargs ssp12b14b_g_tb -debug_all
+echo "\n\n### You can run\nvcs ssp12b14b_tb -debug_all -R -gui"
+echo "\nor ./simv"
